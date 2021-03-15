5c7daf739633c884e3017b88ac2576df66b2d280
rich11berr <rinocraft.tv@gmail.com>
2021-03-15 23:27:46 +0400

    feat: finish TAP task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array == 0 || array === undefined) {
+    return 0;
+  }
+  return Math.min.apply(null,array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array == 0 || array === undefined) {
+    return 0;
 }
-
+  return Math.max.apply(null,array);
+}
 exports.avg = function avg (array) {
-  return 0;
+  if (array == 0 || array === undefined) {
+    return 0;
+}
+  return array.reduce((a,b) => a + b) / array.length;
 }
